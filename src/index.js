f0ea00d33580e191764bad32a65a35c592628f9a
Stanislau Alimpau <alimberg@list.ru>
2018-02-26 13:51:09 +0300

    second

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(preferences) {
+  var count=0;
+  for (var i=0;i<preferences.length;i++){
+    var x=preferences[i];
+    var y=preferences[x-1];
+    var z=preferences[y-1];
+    if (z-1==i) {
+      count++;
+    }
+  }
+  return Math.floor(count/3);
 };
